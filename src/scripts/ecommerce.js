53921ef473939a29ac4f6b131f55e1851f7c4365
luisrossim <luisrossim23@gmail.com>
2025-01-03 00:14:11 -0300

    v1.4

--- src/scripts/ecommerce.js
+++ src/scripts/ecommerce.js
@@ -1,7 +1,6 @@
 import { fetchProdutos } from "./services/ecommerce-crud";
 
-const destaquesSection = document.querySelector("#destaques");
-const novidadesSection = document.querySelector("#novidades");
+const homeItemsSection = document.querySelector("#home-items");
 
 exibirProdutos();
 
@@ -9,27 +8,60 @@ async function exibirProdutos(){
     let produtos = await fetchProdutos();
 
     if (produtos.length > 0) {
-        let produtosDestaques =  produtos.filter(produto => produto.id <= 4);
-        produtosDestaques.forEach(produto => {
+        produtos.forEach(produto => {
             const produtoElement = buildProdutoElement(produto);
-            destaquesSection.appendChild(produtoElement);
+            homeItemsSection.appendChild(produtoElement);
         });
 
-        let produtosNovidades =  produtos.filter(produto => produto.id > 4);
-        produtosNovidades.forEach(produto => {
-            const produtoElement = buildProdutoElement(produto);
-            novidadesSection.appendChild(produtoElement);
-        });
+        handleProductCardHoverEvent();
     }
 }
 
-function buildProdutoElement(produto){
+function buildProdutoElement(produto) {
     const div = document.createElement("div");
-    div.className = "grid grid-cols-1 gap-4 text-center border rounded-md cursor-pointer p-4";
+    div.className = "grid grid-cols-1 gap-2 product-card relative border rounded-md overflow-hidden cursor-pointer p-4";
     div.innerHTML = `
-        <small>${produto.nome}</small>
-        <img width="150px" class="mx-auto" src="${produto.imagem}"/>
-        <p>R$ ${produto.preco}</p>
+        <div class="w-full h-[200px] lg:h-[260px] mx-auto overflow-hidden">
+            <img class="w-full h-full object-cover" src="${produto.imagem}" data-original-src="${produto.imagem}" />
+        </div>
+        <p class="font-thin text-sm md:text-lg">
+            ${produto.nome}
+        </p>
+        <div class="flex flex-col gap-2">
+            <div class="flex items-center font-light gap-2">
+               <p class="line-through text-neutral-500">
+                    R$ ${(produto.preco / (1 - produto.desconto / 100)).toFixed(2)}
+                </p>
+                <p class="text-green-600 text-xs">
+                    ${produto.desconto}% OFF
+                </p> 
+            </div>
+            <p>
+                R$ ${produto.preco}
+            </p>
+        </div>
+        <button type="button" class="absolute top-6 right-6">
+            <i class="fa-regular fa-heart text-neutral-400"></i>
+        </button>
+        <div class="absolute top-2 left-2 text-white text-sm font-light self-start">
+            ${produto.estoque < 3 ? '<p class="rounded-md bg-black px-2 py-1">últimas unidades</p>' : ''}
+        </div>
     `;
     return div;
+}
+
+
+function handleProductCardHoverEvent() {
+    const productCard = document.querySelectorAll(".product-card");
+    productCard.forEach(product => {
+        const image = product.querySelector('img');
+
+        product.addEventListener('mouseenter', () => {
+            image.src = "/zun-js-vanilla/assets/hover.jpg"
+        })
+
+        product.addEventListener('mouseleave', () => {
+            image.src = image.getAttribute("data-original-src");
+        })
+    })
 }
